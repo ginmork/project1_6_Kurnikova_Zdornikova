cf35dfac7a52cbfe8d31e5edf1a9af8d77b79333
Курникова Милана Станиславовна <1899-23@std.pkgh.edu>
2025-09-13 14:29:50 +0300

    №6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
+    println("Введите первое число:")
+    var num1: String = readln()
+    println("Введите второе число:")
+    var num2: String = readln()
+    var result: String = ""
+
+    if (num1.toInt()%2 == 0 && num2.toInt()%2== 0 )
+        println("Нечетного числа не существует")
+
+    else if (num1.toInt()%2 != 0)
+        result = num2 + num1
+
+    else if (num2.toInt()%2 != 0)
+        result = num1 + num2
+        println("нечетное число из введенных цифр" + result)
 
 }
